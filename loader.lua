ecc01f363450eb4e66249cbeca34c1e771b76357
Habuui <galeriadeorigamis@gmail.com>
2026-06-04 22:29:07 -0300

    Add main loader for FE2 TAS Autoplay

---
+++ loader.lua
@@ -0,0 +1,4 @@
+-- FE2 TAS Autoplay - Carregador principal
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Habuui/FE2-TAS-Autoplay-script/main/library.lua"))()
+
+print("✅ FE2 TAS Hub carregado!")
