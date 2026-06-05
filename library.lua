f4544aa06db5f7c57a66f37c50f4a86888921882
Habuui <galeriadeorigamis@gmail.com>
2026-06-05 12:42:22 -0300

    Update library.lua

--- library.lua
+++ library.lua
@@ -2,16 +2,6 @@ local TAS_LIBRARY = {}
 
 function TAS_LIBRARY.getTAS(mapName)
     local tasData = nil
-    -- Cada mapa que você quiser adicionar:
-    if mapName == "nome_do_mapa_1" then
-        tasData = {
-            -- Bloco exportado pelo seu script (use o botão "📤 EXPORTAR TAS")
-        }
-    elseif mapName == "nome_do_mapa_2" then
-        tasData = {
-            -- Outro bloco
-        }
-    end
     return tasData
 end
 
