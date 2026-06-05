09c37394e718b9504b17be99b95d8cc8815dc76e
Habuui <galeriadeorigamis@gmail.com>
2026-06-04 22:30:13 -0300

    Implement TAS library with getTAS function
    Added a library for TAS data management with a function to retrieve TAS data based on map names.

---
+++ library.lua
@@ -0,0 +1,15 @@
+-- Biblioteca de TASs do FE2
+local TAS_LIBRARY = {}
+
+function TAS_LIBRARY.getTAS(mapName)
+    -- Aqui você vai adicionar os dados de cada mapa depois de gravar
+    local tasData = nil
+    if mapName == "Lost_Woods_Easy" then
+        -- tasData = { -- seus frames aqui -- }
+    elseif mapName == "Castle_Tides_Easy" then
+        -- tasData = { -- seus frames aqui -- }
+    end
+    return tasData
+end
+
+return TAS_LIBRARY
