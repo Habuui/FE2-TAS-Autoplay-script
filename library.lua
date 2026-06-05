0897ae2159c0e86309f15290d171184e3585bede
Habuui <galeriadeorigamis@gmail.com>
2026-06-05 02:28:04 -0300

    library.lua

--- library.lua
+++ library.lua
@@ -1,15 +1,11 @@
--- Biblioteca de TASs do FE2
-local TAS_LIBRARY = {}
-
-function TAS_LIBRARY.getTAS(mapName)
-    -- Aqui você vai adicionar os dados de cada mapa depois de gravar
-    local tasData = nil
-    if mapName == "Lost_Woods_Easy" then
-        -- tasData = { -- seus frames aqui -- }
-    elseif mapName == "Castle_Tides_Easy" then
-        -- tasData = { -- seus frames aqui -- }
-    end
-    return tasData
-end
-
+local TAS_LIBRARY = {
+    maps = {
+        ["Costa Retro"] = {
+            -- Aqui vão os eventos (keyframes + inputs) que você exportar
+        },
+        ["Abandoned Facility"] = {
+            -- ...
+        }
+    }
+}
 return TAS_LIBRARY
